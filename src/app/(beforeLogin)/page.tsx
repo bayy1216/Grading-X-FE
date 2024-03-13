fe47a4ca253eec0306022e262efe31d7d37ce9a0
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-03-13 16:02:49 +0900

    [Feat]: 메인화면 텍스트 추가

--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -3,9 +3,12 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <h1>Welcome to Grading X</h1>
+    <main className={styles.container}>
+      <div className={styles.introduction}>
+        <div>Automate Your exam questions</div>
+        <div>
+          Manage your own exam questions and save time
+        </div>
       </div>
     </main>
   );
