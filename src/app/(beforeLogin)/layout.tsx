5e85c7ba679198d5a67b9354d6a9952aeb2a66e9
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-03-20 10:57:22 +0900

    #1 [Feat]: 로그인 modal 추가

---
+++ src/app/(beforeLogin)/layout.tsx
@@ -0,0 +1,12 @@
+import {ReactNode} from "react";
+import style from '@/app/(beforeLogin)/_component/main.module.css';
+
+type Props = { children: ReactNode, modal: ReactNode };
+export default function Layout({ children, modal }: Props) {
+  return (
+    <div className={style.container}>
+      {children}
+      {modal}
+    </div>
+  )
+}
--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -1,28 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
+import Main from "@/app/(beforeLogin)/_component/Main";
 
 export default function Home() {
   return (
-    <main className={styles.container}>
-      <div className={styles.header}>
-        <div className={styles.headerLeft}></div>
-        <div className={styles.headerRight}>
-
-          <Link href={'/dashboard/course'} className={styles.link}>
-            dashboard
-          </Link>
-          <Link href={'/login'} className={styles.link}>
-            로그인
-          </Link>
-        </div>
-      </div>
-      <div className={styles.introduction}>
-        <div>Automate Your exam questions</div>
-        <div>
-          Manage your own exam questions and save time
-        </div>
-      </div>
-    </main>
+    <Main />
   );
 }
