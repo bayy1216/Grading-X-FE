5373141717dccc87f864aa17a55ae46790baca26
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-03-24 21:39:40 +0900

    #1 [Feat]: examDetail 페이지 ui 틀 구현

--- src/app/(afterLogin)/dashboard/course/[courseId]/exam/[examId]/page.tsx
+++ src/app/(afterLogin)/dashboard/course/[courseId]/exam/[examId]/page.tsx
@@ -1,7 +1,43 @@
-export default function Page() {
+import {ExamDetail} from "@/api/exam/exam.response";
+import style from "./page.module.css"
+
+const detailData : ExamDetail = {
+  id: 1,
+  name: "Assignment 1",
+  startTime: "2021-09-01T00:00:00Z",
+  endTime: "2021-09-08T00:00:00Z",
+  description: "This is a description",
+}
+type Props = {
+  params: {
+    examId: string;
+  }
+};
+
+
+export default function Page({params}: Props) {
   return (
-    <div>
-      <h1>Exam</h1>
+    <div className={style.container}>
+      <div className={style.header}>
+        <h1>{detailData.name}</h1>
+        <div className={style.data}>
+          <div className={style.startTime}>
+            {detailData.startTime}
+          </div>
+          <div className={style.endTime}>
+            {detailData.endTime}
+          </div>
+        </div>
+      </div>
+      <div className={style.content}>
+        <h2>Description</h2>
+        <p>{detailData.description}</p>
+
+        <button className={style.startExam}>
+          Start Exam
+        </button>
+      </div>
+
     </div>
   );
 }
