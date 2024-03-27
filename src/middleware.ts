a88ce2453287313565f209a4a049ae6bca35bb46
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-03-27 21:06:38 +0900

    #3 [Feat]: Next 미들웨어 redirect 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,28 @@
-import type { NextRequest } from 'next/server'
+import {NextRequest, NextResponse} from 'next/server'
 import { auth } from "./auth";
+import {func} from "prop-types";
 
 export async function middleware(request: NextRequest) {
   const session = await auth();
-  console.log('middleware session', session);
-  const currentUser = session?.user;
-  console.log('middleware currentUser', currentUser);
+  //console.log('middleware session', session);
 
-  // if (currentUser && !request.nextUrl.pathname.startsWith('/dashboard')) {
-  //   return Response.redirect(new URL('/dashboard', request.url))
-  // }
-  //
-  // if (!currentUser && !request.nextUrl.pathname.startsWith('/login')) {
-  //   return Response.redirect(new URL('/login', request.url))
-  // }
+  //matcher에서 로그인 되어있을때, 로그인페이지로 접근하려고 하면 대시보드로 리다이렉트
+  if (session && isLoginPath(request)) {
+    console.log('로그인페이지로 접근하려고 하면 대시보드로 리다이렉트')
+    return Response.redirect(new URL('/dashboard', request.url))
+  }
+
+  //matcher에서 로그인 안되었을때, 로그인화면이 아닌 다른 페이지로 접근하려고 하면 로그인페이지로 리다이렉트
+  if (!session && !isLoginPath(request)) {
+    console.log('로그인화면이 아닌 다른 페이지로 접근하려고 하면 로그인페이지로 리다이렉트')
+    return Response.redirect(new URL('/login', request.url))
+  }
+
+}
+function isLoginPath(request: NextRequest) {
+  return request.nextUrl.pathname.startsWith('/login')
 }
 
 export const config = {
-  matcher: ['/dashboard', '/login'],
+  matcher: ['/dashboard/:path*', '/login'],
 }
