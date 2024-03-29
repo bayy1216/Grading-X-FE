ee1ea4f104b9026899e47bc6f9e09ae4a82e1f5e
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-03-30 01:53:42 +0900

    #8 [Feat]: 시험 세부페이지 프레임 추가

--- src/pages/dashborad/course/exam/ExamDetailPage.tsx
+++ src/pages/dashborad/course/exam/ExamDetailPage.tsx
@@ -1,7 +1,45 @@
+import {ExamDetail} from "../../../../api/exam/exam.response.ts";
+import style from "./ExamDetailPage.module.css";
+import {useLocation, useNavigate} from "react-router-dom";
+
+const detailData : ExamDetail = {
+  id: 1,
+  name: "Assignment 1",
+  startTime: "2021-09-01T00:00:00Z",
+  endTime: "2021-09-08T00:00:00Z",
+  description: "This is a description",
+}
+
 export default function ExamDetailPage() {
+  const nav = useNavigate();
+  const location = useLocation();
+
+  const onTakeExam = () => {
+    nav(`${location.pathname}/take`);
+  }
+
   return (
-    <div>
-      <h1>Exam Detail Page</h1>
+    <div className={style.container}>
+      <div className={style.header}>
+        <h1>{detailData.name}</h1>
+        <div className={style.data}>
+          <div className={style.startTime}>
+            {detailData.startTime}
+          </div>
+          <div className={style.endTime}>
+            {detailData.endTime}
+          </div>
+        </div>
+      </div>
+      <div className={style.content}>
+        <h2>Description</h2>
+        <p>{detailData.description}</p>
+
+        <button className={style.startExam} onClick={onTakeExam}>
+          Start Exam
+        </button>
+      </div>
+
     </div>
   );
 }
