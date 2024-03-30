e468e354329ac19874ee4a1b5bac17188fce46e8
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-03-30 13:12:17 +0900

    #8 [Feat]: 과제 페이지 헤더 추가

--- src/pages/dashborad/course/exam/ExamPage.tsx
+++ src/pages/dashborad/course/exam/ExamPage.tsx
@@ -4,6 +4,8 @@ import ExamItem from "../../../../components/exam/ExamItem.tsx";
 import {Exam} from "../../../../api/exam/exam.response.ts";
 import {getExams} from "../../../../api/exam/exam.api.ts";
 import {useQuery} from "@tanstack/react-query";
+import ExamHeader from "../../../../components/exam/ExamHeader.tsx";
+import {useState} from "react";
 
 export default function ExamPage() {
   const nav = useNavigate();
@@ -21,8 +23,15 @@ export default function ExamPage() {
     console.log("Clicked");
   }
 
+  const [examTitle, setExamTitle] = useState('');
+  const onSearch = () => {
+    console.log("Search");
+    //useQuery를 다시 요청함
+  }
+
   return (
     <div className={style.container}>
+      <ExamHeader examTitle={examTitle} setExamTitle={(t)=>setExamTitle(t)} onSearch={onSearch}/>
       {
         data?.map((assignment) =>
           <ExamItem key={assignment.id} exam={assignment} onclick={()=> onClick(assignment.id)}/>
