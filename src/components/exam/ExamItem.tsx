f4c27b25148991ce3732e5582549cea4c53a1fbe
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-03-30 13:15:48 +0900

    #8 [Feat]: 과제 item 문구 변경

--- src/components/exam/ExamItem.tsx
+++ src/components/exam/ExamItem.tsx
@@ -25,7 +25,7 @@ export default function ExamItem({exam, onclick}: Props){
         </div>
       </div>
       <div className={style.rightWrapper}>
-        asdasdas
+        수정
         <button className={style.button}>
           <svg width={24} viewBox="0 0 24 24" aria-hidden="true" className="r-18jsvk2 r-4qtqp9 r-yyyyoo r-z80fyv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-19wmn03">
             <g>
