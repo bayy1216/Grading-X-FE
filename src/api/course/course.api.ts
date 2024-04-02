e9113bdfd9fcc256757fa03df704c48f4418e300
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-04-03 02:52:24 +0900

    #10 [Feat]: course 생성 api 추가

--- src/api/course/course.api.ts
+++ src/api/course/course.api.ts
@@ -1,9 +1,17 @@
 import {axiosClient} from "../AxiosClient.ts";
 
 import {Course} from "./course.response.ts";
+import {CourseCreateRequest} from "./course.request.ts";
 
 export async function getCourses() : Promise<Course[]>{
   const res = await axiosClient.get("/api/course");
   return res.data.courseResponses;
 
+}
+
+export async function createCourse(request: CourseCreateRequest) : Promise<number>{
+  const res = await axiosClient.post("/api/course", {
+    ...request
+  });
+  return res.data;
 }
