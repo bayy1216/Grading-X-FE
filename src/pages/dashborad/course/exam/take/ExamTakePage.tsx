8c2fb7819711ee1df5127cf139a1d96f470f8078
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-04-08 00:32:53 +0900

    [Feat]: 응시 페이지 추가

--- src/pages/dashborad/course/exam/take/ExamTakePage.tsx
+++ src/pages/dashborad/course/exam/take/ExamTakePage.tsx
@@ -1,4 +1,9 @@
+import {useLocation} from "react-router-dom";
+
 export default function ExamTakePage() {
+  const location = useLocation();
+  const email = new URLSearchParams(location.search).get("email");
+  console.log(email);
   return (
     <div>
       <h1>Exam Take Page</h1>
