65aa591e4807393f19cc0905846fa245bd160083
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-04-07 20:43:52 +0900

    [Feat]: MemberType util 함수 추가

--- src/api/member/member.response.ts
+++ src/api/member/member.response.ts
@@ -10,6 +10,23 @@ const MemberType = {
   GUEST: 'GUEST',
 } as const;
 
+export function isLoginMember(memberType: MemberType): boolean {
+  return memberType !== MemberType.GUEST;
+}
+
+export function memberTypeToKo(memberType: MemberType): string {
+  switch (memberType) {
+    case MemberType.INSTRUCTOR:
+      return '교수자';
+    case MemberType.STUDENT:
+      return '학생';
+    case MemberType.GUEST:
+      return '게스트';
+    default:
+      throw new Error('Invalid MemberType');
+  }
+}
+
 type MemberType = typeof MemberType[keyof typeof MemberType];
 
 export { MemberType };
