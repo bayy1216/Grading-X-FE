ed8b76f0783db303bb49d6905f25a9dc5bd78e3b
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-04-08 00:48:52 +0900

    [Feat]: 대시보드레이아웃 로그인여부 체크

--- src/pages/dashborad/DashBoardLayout.tsx
+++ src/pages/dashborad/DashBoardLayout.tsx
@@ -2,21 +2,27 @@ import {Outlet, useNavigate} from "react-router-dom";
 import NavMenu from "../../components/dashboard/NavMenu.tsx";
 import style from "./DashBoardLayout.module.css";
 import {MemberContext} from "../Router.tsx";
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 
 export default function DashboardLayout() {
   const {member, isError } = useContext(MemberContext);
   const nav = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
   /**
    * context.member가 null이면 로딩중이므로 Loading...을 반환한다.
    * context.isError가 true이면 로그인 페이지로 이동한다.
    */
-  if(isError) {
-    nav('/login');
-  }
-  if(!member) {
-    return <div>Loading...Layout</div>;
-  }
+  useEffect(() => {
+    if (member !== null) {
+      setIsLoading(false);
+    }else {
+      setIsLoading(true);
+    }
+    if(isError) {
+      nav('/login');
+    }
+  }, [member, isError]);
+
 
   return (
     <div className={style.container}>
@@ -24,7 +30,7 @@ export default function DashboardLayout() {
         <NavMenu/>
       </div>
       <div className={style.rightWrapper}>
-        <Outlet/>
+        {isLoading ? <div>Loading...</div> : <Outlet/>}
       </div>
     </div>
   );
