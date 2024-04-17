f630038b12bf54285a5e1602639add5743f7d166
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-04-17 20:23:25 +0900

    [Feat]: 네비게이션바 resize

--- src/components/dashboard/NavMenu.tsx
+++ src/components/dashboard/NavMenu.tsx
@@ -1,12 +1,25 @@
-import style from "./NavMenu.module.css";
-import {Link, useLocation } from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 import secureLocalStorage from "react-secure-storage";
-import {logout} from "../../api/auth/auth.api.ts";
+import {logout} from "@/api/auth/auth.api.ts";
 import {useQueryClient} from "@tanstack/react-query";
-import {MEMBER} from "../../const/data.ts";
-import {useMemberStore} from "../../store/member.store.ts";
+import {MEMBER} from "@/const/data.ts";
+import {useMemberStore} from "@/store/member.store.ts";
+import {cn} from "@/lib/utils.ts";
+import {buttonVariants} from "@/components/ui/button.tsx";
 
-export default function NavMenu() {
+interface Props {
+  isCollapsed: boolean;
+}
+
+interface NavLink {
+  title: string;
+  icon: string;
+  variant: 'default' | 'ghost';
+  to: string;
+}
+
+
+export default function NavMenu({isCollapsed}: Props) {
   // const [drawerOpen, setDrawerOpen] = useState(false);
   const location = useLocation();
   const memberStore = useMemberStore();
@@ -23,35 +36,117 @@ export default function NavMenu() {
       memberStore.setData(null);
     });
   }
+
+  const links: NavLink[] = [
+    {title: 'Dashboard', icon: "DashboardIcon", variant: 'ghost', to: '/dashboard'},
+    {
+      title: 'Classes',
+      icon: "ClassesIcon",
+      variant: location.pathname.includes('course') ? 'default' : 'ghost',
+      to: '/dashboard/course'
+    },
+    {
+      title: 'Account',
+      icon: "AccountIcon",
+      variant: location.pathname.includes('account') ? 'default' : 'ghost',
+      to: '/dashboard/account'
+    },
+  ]
+
   return (
-    <div className={style.container}>
-      <div className={style.topHeader}>
-        <Link to={'/dashboard/course'} className={style.link}>
-          <h1>Dashboard</h1>
-        </Link>
-      </div>
+    <div
+      data-collapsed={isCollapsed}
+      className="group flex flex-col gap-4 py-2 data-[collapsed=true]:py-2 h-full"
+    >
+      <nav className="grid gap-1 px-2 group-[data-collapsed=true]:justify-center group-[data-collapsed=true]:px-2">
+        {links.map((link, index) =>
+          isCollapsed ? (
+            <>
+              <Link
+                to={link.to}
+                className={cn(
+                  buttonVariants({variant: link.variant, size: "icon"}),
+                  "h-9 w-9",
+                  link.variant === "default" &&
+                  "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
+                )}
+              >
+                <span className="sr-only">{link.title}</span>
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link
+                key={index}
+                to={link.to}
+                className={cn(
+                  buttonVariants({variant: link.variant, size: "sm"}),
+                  link.variant === "default" &&
+                  "dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white",
+                  "justify-start"
+                )}
+              >
+                {link.title}
 
+              </Link>
+            </>
+          )
+        )}
 
+        {isCollapsed ? (
+          <Link to={'/'} onClick={onLogout}
+                className={cn(
+                  buttonVariants({variant: 'ghost', size: "icon"}),
+                  "h-9 w-9",
+                )}>
+            <span className="sr-only">로그아웃</span>
 
-      <div className={style.section}>Classes</div>
-      <ul>
-        <Link to={'/dashboard/course'} className={style.link}>
-          {location.pathname.includes('course') ? <div className={style.current}>클래스 관리 ✓</div> :
-            <li>클래스</li>}
-        </Link>
-
-
-      </ul>
-      <div className={style.section}>Account</div>
-      <ul>
-        <Link to={'/dashboard/account'} className={style.link}>
-          {location.pathname.includes('account') ? <div className={style.current}>계정 관리 ✓</div> :
-            <li>계정</li>}
-        </Link>
-      </ul>
-      <Link to={'/'} onClick={onLogout} className={style.link}>
-        <div className={style.logout}>로그아웃</div>
-      </Link>
+          </Link>
+        ) : (
+          <Link to={'/'} onClick={onLogout}
+                className={cn(
+                  buttonVariants({variant: 'ghost', size: "sm"}),
+                  "justify-start"
+                )}>
+            로그아웃
+          </Link>
+        )}
+      </nav>
     </div>
   )
+
+  //
+  // return (
+  //   <div className={style.container}>
+  //     <div className={style.topHeader}>
+  //       <Link to={'/dashboard/course'} className={style.link}>
+  //         <h1>Dashboard</h1>
+  //       </Link>
+  //     </div>
+  //
+  //
+  //
+  //     <div className={style.section}>Classes</div>
+  //     <ul>
+  //       <Link to={'/dashboard/course'} className={style.link}>
+  //         {location.pathname.includes('course') ? <div className={style.current}>클래스 관리 ✓</div> :
+  //           <li>클래스</li>}
+  //       </Link>
+  //
+  //
+  //     </ul>
+  //     <Separator />
+  //     <div className={style.section}>Account</div>
+  //     <ul>
+  //       <Link to={'/dashboard/account'} className={style.link}>
+  //         {location.pathname.includes('account') ? <div className={style.current}>계정 관리 ✓</div> :
+  //           <li>계정</li>}
+  //       </Link>
+  //     </ul>
+  //     <Separator />
+  //     <Link to={'/'} onClick={onLogout} className={style.link}>
+  //       <div className={style.logout}>로그아웃</div>
+  //     </Link>
+  //   </div>
+  // )
 }
--- src/pages/(afterLogin)/dashborad/DashBoardLayout.tsx
+++ src/pages/(afterLogin)/dashborad/DashBoardLayout.tsx
@@ -1,10 +1,11 @@
 import {Outlet, useNavigate} from "react-router-dom";
 import NavMenu from "../../../components/dashboard/NavMenu.tsx";
-import style from "./DashBoardLayout.module.css";
-import {useEffect} from "react";
-import {useMemberStore} from "../../../store/member.store.ts";
-import {getMemberInfo} from "../../../api/member/member.api.ts";
+import {useEffect, useState} from "react";
+import {useMemberStore} from "@/store/member.store.ts";
+import {getMemberInfo} from "@/api/member/member.api.ts";
 import secureLocalStorage from "react-secure-storage";
+import {ResizableHandle, ResizablePanel, ResizablePanelGroup} from "@/components/ui/resizable.tsx";
+import {cn} from "@/lib/utils.ts";
 
 export default function DashboardLayout() {
   const nav = useNavigate();
@@ -29,14 +30,63 @@ export default function DashboardLayout() {
     }
   }, []);
 
+
+  const layout = localStorage.getItem("react-resizable-panels:layout")
+  const collapsed = localStorage.getItem("react-resizable-panels:collapsed")
+
+  const defaultLayout = layout ? JSON.parse(layout) : [265,1300]
+  const defaultCollapsed = collapsed ? JSON.parse(collapsed) : false
+  const [isCollapsed, setIsCollapsed] = useState(defaultCollapsed)
+  const navCollapsedSize =4;
+
   return (
-    <div className={style.container}>
-      <div className={style.leftWrapper}>
-        <NavMenu/>
-      </div>
-      <div className={style.rightWrapper}>
-        <Outlet />
-      </div>
-    </div>
-  );
+      <ResizablePanelGroup
+        direction="horizontal"
+        onLayout={(sizes: number[]) => {
+          localStorage.setItem("react-resizable-panels:layout", JSON.stringify(sizes));
+        }}
+        className="h-full items-stretch"
+      >
+        <ResizablePanel
+          defaultSize={defaultLayout[0]}
+          collapsedSize={navCollapsedSize}
+          collapsible={true}
+          minSize={15}
+          maxSize={20}
+          onCollapse={() => {
+            console.log("onCollapse", isCollapsed);
+            setIsCollapsed(true);
+            localStorage.setItem("react-resizable-panels:collapsed", JSON.stringify(true));
+          }}
+          onExpand={() => {
+            console.log("onExpand", isCollapsed);
+            setIsCollapsed(false);
+            localStorage.setItem("react-resizable-panels:collapsed", JSON.stringify(false));
+          }}
+          className={cn(
+            isCollapsed &&
+            "min-w-[50px] transition-all duration-300 ease-in-out h-full"
+          )}
+        >
+          <NavMenu isCollapsed={isCollapsed} />
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={defaultLayout[1]} minSize={30}>
+          <div className="h-dvh">
+          <Outlet  />
+          </div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
+  )
+
+  // return (
+  //   <div className={style.container}>
+  //     <div className={style.leftWrapper}>
+  //       <NavMenu/>
+  //     </div>
+  //     <div className={style.rightWrapper}>
+  //       <Outlet />
+  //     </div>
+  //   </div>
+  // );
 }
