2f6c9996efa2666b606704eb30efc5116d8f6cc7
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-05-09 13:16:30 +0900

    [Feat]: (시험 응시, 제출) 상태관리, API 구현

--- src/pages/(beforeLogin)/GuestExamTakePage.tsx
+++ src/pages/(beforeLogin)/GuestExamTakePage.tsx
@@ -1,14 +1,17 @@
-import {useLocation, useNavigate, } from "react-router-dom";
+import {useLocation, useNavigate,} from "react-router-dom";
 import {useQuery} from "@tanstack/react-query";
 import {QuestionsResponse} from "@/api/question/question.response.ts";
 import {MINUTE_5, QUESTIONS} from "@/const/data.ts";
 import {getQuestionsByExamIdForSolve} from "@/api/question/question.api.ts";
 import ExamQuestion from "@/components/exam/ExamQuestion.tsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Input} from "@/components/ui/input.tsx";
 import {Label} from "@/components/ui/label.tsx";
 import {Separator} from "@/components/ui/separator.tsx";
 import {Button} from "@/components/ui/button.tsx";
+import {ExamSaveRequest, MemberExamAnswer} from "@/api/exam/exam.request.ts";
+import {GreenButton} from "@/components/ui/GreenButton.tsx";
+import {saveExamAnswer} from "@/api/exam/exam.api.ts";
 
 export default function GuestExamTakePage() {
   const location = useLocation();
@@ -35,24 +38,79 @@ export default function GuestExamTakePage() {
       </div>
     )
   }
+
+  const [examAnswers, setExamAnswers]
+    = useState<MemberExamAnswer[]>([]);
+
+  /**
+   * questions가 변경되면 examAnswers를 초기화
+   */
+  useEffect(() => {
+    if (questions) {
+      setExamAnswers(questions.map((question) => ({
+        questionId: question.id,
+        answer: "",
+      })));
+    }
+  }, [questions]);
+
+  /**
+   * 답안이 변경되면 examAnswers를 업데이트
+   */
+  const onAnswerChange = (questionId: number, answer: string) => {
+    setExamAnswers((prev) => {
+      return prev.map((prevAnswer) => {
+        if (prevAnswer.questionId !== questionId) {
+          return prevAnswer;
+        }
+        return {
+          ...prevAnswer,
+          answer,
+        };
+      });
+    });
+  }
+
+  const onSubmit = async () => {
+    const request : ExamSaveRequest = {
+      email: guestEmail,
+      answers : examAnswers
+    }
+    await saveExamAnswer(examId, request);
+  }
+
   return (
     <div className="flex flex-col items-center">
       <div className="w-[1200px]">
+        <div className="flex flex-row justify-between items-center">
+          <div className="flex flex-col space-y-1.5 my-4">
+            <Label htmlFor="name" className="text-left">
+              응시 이메일
+            </Label>
+            <Input
+              name="guestEmail" placeholder="응시 이메일"
+              value={guestEmail}
+              onChange={(e) => setGuestEmail(e.target.value)}
+              className="w-[400px]"
+            />
+          </div>
 
-        <div className="flex flex-col space-y-1.5 my-4">
-          <Label htmlFor="name" className="text-left">
-            응시 이메일
-          </Label>
-          <Input
-            name="guestEmail" placeholder="응시 이메일"
-            value={guestEmail}
-            onChange={(e) => setGuestEmail(e.target.value)}
-            className="w-[400px]"
-          />
+          <GreenButton onClick={onSubmit}>
+            제출하기
+          </GreenButton>
         </div>
+
+
         <Separator className="mb-4"/>
         {questions?.map((question) => (
-          <ExamQuestion questions={question} key={question.id}/>
+          <ExamQuestion
+            questions={question}
+            answer={examAnswers.find((answer) => answer.questionId === question.id)?.answer!}
+            onAnswerChange={(answer) => {
+              onAnswerChange(question.id, answer);
+            }}
+            key={question.id}
+          />
         ))}
       </div>
 
