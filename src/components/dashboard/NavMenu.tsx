0711e70edc08a473197761dd632f6a1399a4b613
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-05-13 19:32:02 +0900

    [Fix]: NavMenu key 추가

--- src/components/dashboard/NavMenu.tsx
+++ src/components/dashboard/NavMenu.tsx
@@ -63,83 +63,86 @@ export default function NavMenu({isCollapsed}: Props) {
 
   return (
     <TooltipProvider>
-    <div
-      data-collapsed={isCollapsed}
-      className="group flex flex-col gap-4 py-2 data-[collapsed=true]:py-2 h-full"
-    >
-      <nav className="grid gap-1 px-2 group-[data-collapsed=true]:justify-center group-[data-collapsed=true]:px-2">
-        {links.map((link, index) =>
-          isCollapsed ? (
-            <Tooltip key={index} delayDuration={0}>
-            <TooltipTrigger asChild>
-              <Link
-                to={link.to}
-                className={cn(
-                  buttonVariants({variant: link.variant, size: "icon"}),
-                  "h-9 w-9",
-                  link.variant === "default" &&
-                  "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
-                )}
-              >
-                <link.icon className="h-4 w-4"/>
-                <span className="sr-only">{link.title}</span>
-              </Link>
-            </TooltipTrigger>
+      <div
+        data-collapsed={isCollapsed}
+        className="group flex flex-col gap-4 py-2 data-[collapsed=true]:py-2 h-full"
+      >
+        <nav className="grid gap-1 px-2 group-[data-collapsed=true]:justify-center group-[data-collapsed=true]:px-2">
+          {links.map((link, index) =>
+            isCollapsed ? (
+              <div key={index}>
+                <Tooltip key={index} delayDuration={0}>
+                  <TooltipTrigger asChild>
+                    <Link
+                      key={index}
+                      to={link.to}
+                      className={cn(
+                        buttonVariants({variant: link.variant, size: "icon"}),
+                        "h-9 w-9",
+                        link.variant === "default" &&
+                        "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
+                      )}
+                    >
+                      <link.icon className="h-4 w-4"/>
+                      <span className="sr-only">{link.title}</span>
+                    </Link>
+                  </TooltipTrigger>
+                  <TooltipContent side="right" className="flex items-center gap-4">
+                    {link.title}
+                  </TooltipContent>
+                </Tooltip>
+              </div>
+            ) : (
+
+                <Link
+                  key={index}
+                  to={link.to}
+                  className={cn(
+                    buttonVariants({variant: link.variant, size: "sm"}),
+                    link.variant === "default" &&
+                    "dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white",
+                    "justify-start"
+                  )}
+                >
+                  <link.icon className="h-4 w-4 mr-2"/>
+                  {link.title}
+
+                </Link>
+
+            )
+          )}
+          <Separator/>
+
+          {isCollapsed ? (
+            <Tooltip delayDuration={0}>
+              <TooltipTrigger asChild>
+                <Link to={'/'} onClick={onLogout}
+                      className={cn(
+                        buttonVariants({variant: 'ghost', size: "icon"}),
+                        "h-9 w-9",
+                      )}>
+
+                  <LogOut className="h-4 w-4"/>
+                  <span className="sr-only">로그아웃</span>
+
+                </Link>
+              </TooltipTrigger>
               <TooltipContent side="right" className="flex items-center gap-4">
-                {link.title}
+                로그아웃
               </TooltipContent>
             </Tooltip>
           ) : (
-            <>
-              <Link
-                key={index}
-                to={link.to}
-                className={cn(
-                  buttonVariants({variant: link.variant, size: "sm"}),
-                  link.variant === "default" &&
-                  "dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white",
-                  "justify-start"
-                )}
-              >
-                <link.icon className="h-4 w-4 mr-2"/>
-                {link.title}
-
-              </Link>
-            </>
-          )
-        )}
-        <Separator/>
-
-        {isCollapsed ? (
-          <Tooltip delayDuration={0}>
-            <TooltipTrigger asChild>
-          <Link to={'/'} onClick={onLogout}
-                className={cn(
-                  buttonVariants({variant: 'ghost', size: "icon"}),
-                  "h-9 w-9",
-                )}>
-
-            <LogOut className="h-4 w-4"/>
-            <span className="sr-only">로그아웃</span>
-
-          </Link>
-            </TooltipTrigger>
-            <TooltipContent side="right" className="flex items-center gap-4">
+            <Link to={'/'} onClick={onLogout}
+                  className={cn(
+                    buttonVariants({variant: 'ghost', size: "sm"}),
+                    "justify-start"
+                  )}>
+              <LogOut className="h-4 w-4 mr-2"/>
               로그아웃
-            </TooltipContent>
-          </Tooltip>
-        ) : (
-          <Link to={'/'} onClick={onLogout}
-                className={cn(
-                  buttonVariants({variant: 'ghost', size: "sm"}),
-                  "justify-start"
-                )}>
-            <LogOut className="h-4 w-4 mr-2"/>
-            로그아웃
-          </Link>
-        )}
-      </nav>
-    </div>
+            </Link>
+          )}
+        </nav>
+      </div>
     </TooltipProvider>
   );
 }
